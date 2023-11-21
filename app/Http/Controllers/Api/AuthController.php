acc34c2acddab74e9864037e64f08e385b4d1b67
Kenechukwu Mokwenye <mokwenyekene1@yahoo.com>
2023-11-21 17:09:08 +0100

    feat: app file

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use auth;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 
@@ -14,61 +15,65 @@ class AuthController extends Controller
     public function _construct(){
         $this->middleware('auth:api', ['expect' => ['login', 'register']]);
     }
-    /**
-     * @OA\Post(
-     *     path="/api/auth/register",
-     *     tags={"Register"},
-     *     summary="Register a new user",
-     *     description="Registers a new user with the provided information given.",
-     *     operationId="registerUser",
-     *     @OA\RequestBody(
-     *         required=true,
-     *         @OA\MediaType(
-     *             mediaType="application/json",
-     *             @OA\Schema(
-     *                 @OA\Property(
-     *                     property="name",
-     *                     type="string",
-     *                     description="User's name",
-     *                     example="John Doe"
-     *                 ),
-     *                 @OA\Property(
-     *                     property="email",
-     *                     type="string",
-     *                     format="email",
-     *                     description="User's email",
-     *                     example="john@example.com"
-     *                 ),
-     *                 @OA\Property(
-     *                     property="password",
-     *                     type="string",
-     *                     description="User's password",
-     *                     example="password123"
-     *                 ),
-     *             )
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response="201",
-     *         description="User registered successfully",
-     *         @OA\JsonContent(
-     *             @OA\Property(property="message", type="string", example="User registered successfully")
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response="422",
-     *         description="Validation errors",
-     *         @OA\JsonContent(
-     *             @OA\Property(property="message", type="string", example="Validation errors"),
-     *             @OA\Property(
-     *                 property="errors",
-     *                 type="object",
-     *                 @OA\Property(property="field_name", type="array", @OA\Items(type="string", example="The field is required."))
-     *             )
-     *         )
-     *     )
-     * )
-     */
+
+
+/**
+ * @OA\Post(
+ *     path="/api/auth/register",
+ *     tags={"Register"},
+ *     summary="Register a new user",
+ *     description="Registers a new user with the provided information given.",
+ *     operationId="registerUser",
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                     property="name",
+ *                     type="string",
+ *                     description="User's name",
+ *                     example="John Doe"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="email",
+ *                     type="string",
+ *                     format="email",
+ *                     description="User's email",
+ *                     example="john@example.com"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="password",
+ *                     type="string",
+ *                     description="User's password",
+ *                     example="password123"
+ *                 ),
+ *             )
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response="201",
+ *         description="User registered successfully",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="message", type="string", example="User registered successfully")
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response="422",
+ *         description="Validation errors",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="message", type="string", example="Validation errors"),
+ *             @OA\Property(
+ *                 property="errors",
+ *                 type="object",
+ *                 @OA\Property(property="field_name", type="array", @OA\Items(type="string", example="The field is required."))
+ *             )
+ *         )
+ *     )
+ * )
+ */
+
+ 
 
     public function register(Request $request){
 
@@ -107,6 +112,62 @@ class AuthController extends Controller
             }
     }   }
 
+    /**
+ * @OA\Post(
+ *     path="/api/auth/login",
+ *     tags={"Login"},
+ *     summary="User Login",
+ *     description="Logs in a user with the provided credentials.",
+ *     operationId="loginUser",
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                     property="email",
+ *                     type="string",
+ *                     format="email",
+ *                     description="User's email",
+ *                     example="john@example.com"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="password",
+ *                     type="string",
+ *                     description="User's password",
+ *                     example="password123456"
+ *                 ),
+ *             )
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response="200",
+ *         description="Login successful",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="token", type="string", example="eyJ0eXAiOiJKV1QiLCJhbGciOi..."),
+ *             @OA\Property(property="token_type", type="string", example="Bearer"),
+ *             @OA\Property(property="expires_in", type="integer", example=3600),
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response="401",
+ *         description="Unauthorized",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="error", type="string", example="Unauthorized"),
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response="400",
+ *         description="Validation errors",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="status", type="integer", example=400),
+ *             @OA\Property(property="errors", type="object", example={"email": {"The email field is required."}}),
+ *         )
+ *     )
+ * )
+ */
+    
+
     public function login(Request $request){
         $validator = Validator::make($request->all(), [
 
@@ -130,22 +191,71 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60,
+            'expires_in' => JWTAuth::factory()->getTTL() * 60,
             'user' => auth() ->user()
         ]);
     } 
+
+    
+/**
+ * @OA\Post(
+ *     path="/api/auth/refresh",
+ *     tags={"Auth"},
+ *     summary="Refresh Access Token",
+ *     description="Refreshes the access token.",
+ *     operationId="refreshToken",
+ *     security={
+ *         {"bearerAuth": {}}
+ *     },
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                     property="refresh_token",
+ *                     type="string",
+ *                     description="User's refresh token",
+ *                     example="your_refresh_token_here"
+ *                 ),
+ *             )
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response="200",
+ *         description="Token refreshed successfully",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="access_token", type="string", example="new_access_token"),
+ *             @OA\Property(property="token_type", type="string", example="bearer"),
+ *             @OA\Property(property="expires_in", type="integer", example="3600")
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response="401",
+ *         description="Unauthorized",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="message", type="string", example="Unauthenticated")
+ *         )
+ *     )
+ * )
+ */
+ 
+    public function refresh(){
+        return $this->createNewToken(auth('api')->refresh());
+    }
     
 
-    public function refresh()
-{
-    return $this->CreateNewToken(auth()->refresh());
-}
 
 
+    
 
     public function me (){
         return response()->json(auth()->user());
+        
+        
     }
+
+    
 
     public function logout(){
         auth()->logout();
